eff56a6111afe08f3f35696e8c4c9d8a3b9db341
Mohamed Ahmad Aamer <mohamedamer283@gmail.com>
2023-12-16 16:16:07 +0200

    Create new component for products

---
+++ src/app/Products/products.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: '',
+  template: '',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent {
+  title = 'Products';
+}
