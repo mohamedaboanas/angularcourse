ec7a45fc63ac83b95d2f0b88eeda07a338bf3058
Mohamed Ahmad Aamer <mohamedamer283@gmail.com>
2023-12-19 22:22:36 +0200

    Add types of directives to app

--- src/app/Products/products.component.ts
+++ src/app/Products/products.component.ts
@@ -1,10 +1,13 @@
-import {Component} from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
-  title = 'Products';
+  title: string = 'Products';
+  isValid: boolean = true;
+  Status: string = 'Success';
+  Num:number = 0;
 }
