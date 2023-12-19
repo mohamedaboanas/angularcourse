fb7ffa6eb57a169ff7464d7d176529e47e735c72
Mohamed Ahmad Aamer <mohamedamer283@gmail.com>
2023-12-19 14:01:17 +0200

    new section for databinding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  title: string = `hi, angular`;
+  _ImgUrl: string = '';
+  _ImgName: string = 'Angular';
+  _Id: string | number = 5;
+  _Width: number = 50;
+  _height:number = 20;
 
+  ngOnInit(): void {}
+  _Msg() {
+    alert(`This is message for you to say welcome to our angular course`);
+  }
 }
